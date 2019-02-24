3a45fbaaec8c5a5c1baca70b827753aebea4b7a3
Amir Savand <amir@savandbros.com>
2019-02-24 13:36:42 +0330

    Add sidebar toggle functionality

--- app/static/app.js
+++ app/static/app.js
@@ -1,4 +1,23 @@
+/**
+ * Sidebar elements
+ * @type {{left: Element, right: Element}}
+ */
+const sidebars = {
+  left: document.getElementsByClassName("sidebar-left")[0],
+  right: document.getElementsByClassName("sidebar-right")[0],
+};
+
+/**
+ * Toggle sidebars, used for sidebar buttons
+ * @param sidebar {string}
+ */
+const toggleSidebar = (sidebar) => {
+  sidebars[sidebar].classList.toggle('sidebar-open');
+};
+
+/**
+ * On page load
+ */
 (() => {
-  let main = document.getElementById("main");
-  main.style.minHeight = window.innerHeight - 72 + "px";
+  document.getElementById("main").style.minHeight = window.innerHeight - 72 + "px";
 })();
