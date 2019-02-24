e061f5399be7ca317c22720c8807a2171846e04e
Amir Savand <amir@savandbros.com>
2019-02-24 15:36:20 +0330

    Remove old layout scripts

--- app/static/app.js
+++ app/static/app.js
@@ -1,23 +1,7 @@
 /**
- * Sidebar elements
- * @type {{left: Element, right: Element}}
- */
-const sidebars = {
-  left: document.getElementsByClassName("sidebar-left")[0],
-  right: document.getElementsByClassName("sidebar-right")[0],
-};
-
-/**
  * Toggle sidebars, used for sidebar buttons
  * @param sidebar {string}
  */
 const toggleSidebar = (sidebar) => {
-  sidebars[sidebar].classList.toggle('sidebar-open');
+  document.getElementsByClassName("sidebar-" + sidebar)[0].classList.toggle('sidebar-open');
 };
-
-/**
- * On page load
- */
-(() => {
-  document.getElementById("main").style.minHeight = window.innerHeight - 72 + "px";
-})();
